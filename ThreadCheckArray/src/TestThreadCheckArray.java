bc6ec912b5b55e95ce95c52b8bad0ea338d45fff
haimb <haimb@DESKTOP-803PJLO>
2021-03-21 12:04:02 +0200

    ArrayList

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestThreadCheckArray {
@@ -6,11 +7,14 @@ public class TestThreadCheckArray {
 			Thread thread1, thread2;
 			System.out.println("Enter array size");
 			int num  = input.nextInt();
-			int [] array = new int[num];
+			ArrayList<Integer> array = new ArrayList<>(num);
 			System.out.println("Enter numbers for array");
 			
-			for (int index = 0; index < num; index++) 
-				array[index] = input.nextInt();
+			
+			for(Integer p: array)
+				p = input.nextInt();
+			//for (int index = 0; index < num; index++) 
+			//	array[index] = input.nextInt();
 			
 			System.out.println("Enter number");
 			num = input.nextInt();
